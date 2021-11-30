a018ccf8d659bd80f9acb6aaf7dfb75e8793aa40
Giang Hoang <gianghoang.hg.dn@gmail.com>
2021-11-30 13:24:34 +0700

    Chapter 7 : Sign Up

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_160026) do
-
-  create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "birthday"
+    t.string "gender"
   end
 
 end
