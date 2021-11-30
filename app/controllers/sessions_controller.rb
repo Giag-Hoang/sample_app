21afbf855d2b85bd8be1a34a2bc14ca2d6a0e6fa
Giang Hoang <gianghoang.hg.dn@gmail.com>
2021-11-30 13:24:34 +0700

    Chapter 9 : Advanced Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
-      log_in user
-      redirect_to user
+      operation(user)
     else
       flash.now[:danger] = t("app.controllers.sessions.password_error")
       render :new
